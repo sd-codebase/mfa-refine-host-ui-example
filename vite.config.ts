54907fd019b06590c7d371d18f509941bb574a45
Krishna <krishna.dhas021815@gmail.com>
2025-01-18 16:23:37 +0530

    feat: added support for husky modular scss and paths in tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import * as path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {},
+    },
+  },
+  server: {
+    port: 5000,
+  },
+  resolve: {
+    alias: {
+      '@host': path.resolve(__dirname, './src'),
+      '@host/components': path.resolve(__dirname, './src/components'),
+      '@host/pages': path.resolve(__dirname, './src/pages'),
+      '@host/styles': path.resolve(__dirname, './src/styles'),
+      '@host/types': path.resolve(__dirname, './src/types'),
+    },
+  },
 });
